d6fed63a3487fe260a91df9ae9a3098b0cc3d912
Dredd Villarba Domasian <Domasiandredd@gmail.com>
2025-06-10 23:14:25 +0800

    delete update

--- php/deleteCustomer.php
+++ php/deleteCustomer.php
@@ -2,6 +2,11 @@
     include('dbconnect.php');
     $id = $_GET['id'];
 
+    $sql1 = "DELETE FROM rental WHERE customerID = ?";
+    $stmt1 = $con->prepare($sql1);
+    $stmt1->bind_param("i", $id);
+    $stmt1->execute();
+
     $sql = "DELETE FROM customerinfo WHERE customerID = ?";
     $stmt = $con->prepare($sql);
     $stmt->bind_param("i", $id);
